14d92d207b15812cc01404dbfa8f8bdf9aa5dd50
IzumiShaka-desu <shakaaji29@gmail.com>
2023-01-11 09:52:01 +0700

    add close button on footer modal

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -124,6 +124,7 @@
 		dialogContent += "<div class='modal-footer'>";
 		dialogContent += "<div class='row'>";
 		dialogContent += "<div class='col-md-12'>";
+		dialogContent += '<button type="button" class="btn btn-sm btn-light" data-dismiss="modal">Close</button>';
 		dialogContent += "<a class='btn btn-sm btn-primary' href='<?= base_url('checksheet') ?>/" + schedule.id_checksheet + "' target='_blank'>Open Checksheet</a>";
 		dialogContent += "</div>";
 		dialogContent += "</div>";
