505d27e04bc189f1df13d3a402529ce65e38cb53
IzumiShaka-desu <shakaaji29@gmail.com>
2023-01-17 08:06:36 +0700

    change login validation and add method get id and name

--- application/models/m_login.php
+++ application/models/m_login.php
@@ -10,16 +10,47 @@ class M_login extends CI_Model
 		$this->load->database();
 	}
 
-	public function validate($username, $password)
+	// public function validate($username, $password)
+	// {
+	// 	//validate login
+	// 	$this->db->where('username', $username);
+	// 	$this->db->where('password', $password);
+	// 	$query = $this->db->get('users');
+	// 	if ($query->num_rows() == 1) {
+	// 		return true;
+	// 	} else {
+	// 		return false;
+	// 	}
+	// }
+
+	/**
+	 * Validate the login's data with the database
+	 * @param string $username
+	 * @param string $password
+	 * @return void
+	 */
+
+	/*Check Login*/
+	function validate($username, $password)
 	{
-		//validate login
-		$this->db->where('username', $username);
 		$this->db->where('password', $password);
-		$query = $this->db->get('users');
-		if ($query->num_rows() == 1) {
-			return true;
-		} else {
-			return false;
-		}
+		$this->db->where('username', $username);
+
+		$query = $this->db->get('user');
+		return $query->result();
+	}
+
+	/*Get Session values */
+
+	function get_id($username, $password)
+	{
+		$this->db->where('password', $password);
+		$this->db->where('username', $username);
+		return $this->db->get('user')->result();
+	}
+
+	function getName($id)
+	{
+		return $this->db->get_where('user', array('id_user' => $id))->result_array();
 	}
 }
