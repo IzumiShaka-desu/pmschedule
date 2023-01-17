e6719241562cadd5673e230e732ef8e3a6f5babf
IzumiShaka-desu <shakaaji29@gmail.com>
2023-01-17 08:06:52 +0700

    change validate procedure

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -24,21 +24,52 @@ class Login extends CI_Controller
 		//validate login
 		$username = $this->input->post('username');
 		$password = $this->input->post('password');
-		$result = $this->m_login->validate($username, $password);
-		if ($result) {
-			//if login success
-			//set session
-			$session_data = array(
-				'username' => $username,
-				// 'name' => $result['name'],
-				'logged_in' => true
-			);
-			$this->session->set_userdata($session_data);
-			// redirect('');
-			echo '<script>  window.location.href = "' . base_url() . '"; </script>';
-		} else {
-			//if login failed
-			$this->session->set_flashdata('error', 'Invalid Username or Password');
+		// $result = $this->m_login->validate($username, $password);
+		// if ($result) {
+		// 	//if login success
+		// 	//set session
+		// 	$session_data = array(
+		// 		'username' => $username,
+		// 		// 'name' => $result['name'],
+		// 		'logged_in' => true
+		// 	);
+		// 	$this->session->set_userdata($session_data);
+		// 	// redirect('');
+		// 	echo '<script>  window.location.href = "' . base_url() . '"; </script>';
+		// } else {
+		// 	//if login failed
+		// 	$this->session->set_flashdata('error', 'Invalid Username or Password');
+		// 	redirect('login');
+		// }
+		$is_valid = $this->m_login->validate($username, $password);
+
+		if ($is_valid)/*If valid username and password set */ {
+			$get_id = $this->m_login->get_id($username, $password);
+
+			foreach ($get_id as $val) {
+				$name = $val->name;
+				$password = $val->password;
+				$level = $val->level;
+				$id_user = $val->id_user;
+				$id_department = $val->id_department;
+				$id_section = $val->id_section;
+
+				$data = array(
+					'name' => $name,
+					'password' => $password,
+					'level' => $level,
+					'is_logged_in' => true,
+					'id_user' => $id_user,
+					'id_department' => $id_department,
+					'id_section' => $id_section
+				);
+
+				$this->session->set_userdata($data);
+				redirect(base_url('dashboard/'));
+			}
+		} else // incorrect username or password
+		{
+			$this->session->set_flashdata('msg1', 'Username or Password Incorrect!');
 			redirect('login');
 		}
 	}
